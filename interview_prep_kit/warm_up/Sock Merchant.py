ba612b2803ac5eb04b25532800f5cfa86dff0ad3
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-22 15:24:44 +0300

    Create Sock Merchant.py

---
+++ interview_prep_kit/warm_up/Sock Merchant.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    dct = {}
+    for i in range(n):
+        if ar[i] not in dct:
+            dct[ar[i]] = 1
+        else:
+            dct[ar[i]] += 1
+    return sum([k//2 for k in dct.values()])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
